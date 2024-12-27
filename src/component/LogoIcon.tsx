44da19f078f15cd49606bac1230c260562f5d15f
Jisun Park <jisunpark.t@gmail.com>
2024-12-27 13:19:27 +0900

    fix: svg 속성 카멜케이스로 변환

---
+++ src/component/LogoIcon.tsx
@@ -0,0 +1,13 @@
+export default function LogoIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" fill="none">
+      <path
+        d="M13.9999 2.33333V25.6667M22.2495 5.75042L5.75034 22.2496M25.6666 14H2.33325M22.2495 22.2496L5.75034 5.75042"
+        stroke="#3179B7"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
