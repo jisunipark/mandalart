3b9ee42673c1b21e0260bcda699d8f0de5cd1408
Jisun Park <jisunpark.t@gmail.com>
2024-12-28 17:52:23 +0900

    feat: 디폴트 데이터 및 타입 구축

---
+++ src/data/default.tsx
@@ -0,0 +1,325 @@
+type SubGoal = {
+  id: number;
+  subGoal: string;
+};
+
+type KeyGoal = {
+  id: string;
+  keyGoal: string;
+  subGoals: SubGoal[];
+};
+
+type GoalData = {
+  coreGoal: string;
+  keyGoals: KeyGoal[];
+};
+
+export const goalData = {
+  coreGoal: "",
+  keyGoals: [
+    {
+      id: 1,
+      keyGoal: "",
+      subGoals: [
+        {
+          id: 1,
+          subGoal: "",
+        },
+        {
+          id: 2,
+          subGoal: "",
+        },
+        {
+          id: 3,
+          subGoal: "",
+        },
+        {
+          id: 4,
+          subGoal: "",
+        },
+        {
+          id: 5,
+          subGoal: "",
+        },
+        {
+          id: 6,
+          subGoal: "",
+        },
+        {
+          id: 7,
+          subGoal: "",
+        },
+        {
+          id: 8,
+          subGoal: "",
+        },
+      ],
+    },
+    {
+      id: 2,
+      keyGoal: "",
+      subGoals: [
+        {
+          id: 1,
+          subGoal: "",
+        },
+        {
+          id: 2,
+          subGoal: "",
+        },
+        {
+          id: 3,
+          subGoal: "",
+        },
+        {
+          id: 4,
+          subGoal: "",
+        },
+        {
+          id: 5,
+          subGoal: "",
+        },
+        {
+          id: 6,
+          subGoal: "",
+        },
+        {
+          id: 7,
+          subGoal: "",
+        },
+        {
+          id: 8,
+          subGoal: "",
+        },
+      ],
+    },
+    {
+      id: 3,
+      keyGoal: "",
+      subGoals: [
+        {
+          id: 1,
+          subGoal: "",
+        },
+        {
+          id: 2,
+          subGoal: "",
+        },
+        {
+          id: 3,
+          subGoal: "",
+        },
+        {
+          id: 4,
+          subGoal: "",
+        },
+        {
+          id: 5,
+          subGoal: "",
+        },
+        {
+          id: 6,
+          subGoal: "",
+        },
+        {
+          id: 7,
+          subGoal: "",
+        },
+        {
+          id: 8,
+          subGoal: "",
+        },
+      ],
+    },
+    {
+      id: 4,
+      keyGoal: "",
+      subGoals: [
+        {
+          id: 1,
+          subGoal: "",
+        },
+        {
+          id: 2,
+          subGoal: "",
+        },
+        {
+          id: 3,
+          subGoal: "",
+        },
+        {
+          id: 4,
+          subGoal: "",
+        },
+        {
+          id: 5,
+          subGoal: "",
+        },
+        {
+          id: 6,
+          subGoal: "",
+        },
+        {
+          id: 7,
+          subGoal: "",
+        },
+        {
+          id: 8,
+          subGoal: "",
+        },
+      ],
+    },
+    {
+      id: 5,
+      keyGoal: "",
+      subGoals: [
+        {
+          id: 1,
+          subGoal: "",
+        },
+        {
+          id: 2,
+          subGoal: "",
+        },
+        {
+          id: 3,
+          subGoal: "",
+        },
+        {
+          id: 4,
+          subGoal: "",
+        },
+        {
+          id: 5,
+          subGoal: "",
+        },
+        {
+          id: 6,
+          subGoal: "",
+        },
+        {
+          id: 7,
+          subGoal: "",
+        },
+        {
+          id: 8,
+          subGoal: "",
+        },
+      ],
+    },
+    {
+      id: 6,
+      keyGoal: "",
+      subGoals: [
+        {
+          id: 1,
+          subGoal: "",
+        },
+        {
+          id: 2,
+          subGoal: "",
+        },
+        {
+          id: 3,
+          subGoal: "",
+        },
+        {
+          id: 4,
+          subGoal: "",
+        },
+        {
+          id: 5,
+          subGoal: "",
+        },
+        {
+          id: 6,
+          subGoal: "",
+        },
+        {
+          id: 7,
+          subGoal: "",
+        },
+        {
+          id: 8,
+          subGoal: "",
+        },
+      ],
+    },
+    {
+      id: 7,
+      keyGoal: "",
+      subGoals: [
+        {
+          id: 1,
+          subGoal: "",
+        },
+        {
+          id: 2,
+          subGoal: "",
+        },
+        {
+          id: 3,
+          subGoal: "",
+        },
+        {
+          id: 4,
+          subGoal: "",
+        },
+        {
+          id: 5,
+          subGoal: "",
+        },
+        {
+          id: 6,
+          subGoal: "",
+        },
+        {
+          id: 7,
+          subGoal: "",
+        },
+        {
+          id: 8,
+          subGoal: "",
+        },
+      ],
+    },
+    {
+      id: 8,
+      keyGoal: "",
+      subGoals: [
+        {
+          id: 1,
+          subGoal: "",
+        },
+        {
+          id: 2,
+          subGoal: "",
+        },
+        {
+          id: 3,
+          subGoal: "",
+        },
+        {
+          id: 4,
+          subGoal: "",
+        },
+        {
+          id: 5,
+          subGoal: "",
+        },
+        {
+          id: 6,
+          subGoal: "",
+        },
+        {
+          id: 7,
+          subGoal: "",
+        },
+        {
+          id: 8,
+          subGoal: "",
+        },
+      ],
+    },
+  ],
+};
